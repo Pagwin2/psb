0e4613f27cbaa096ababc8e123fdd3a618672519
Pagwin <pgp2024@pagwin.xyz>
2025-07-24 19:39:44 -0400

    began scaffolding down to markdown parser

--- app/IR.hs
+++ app/IR.hs
@@ -2,13 +2,36 @@ module IR where
 
 import Data.Text
 
--- Html and Math tags come with their data because they are leaves for us
--- We aren't parsing that if we can avoid it
-data Tag = Heading {level :: Int} | Paragraph | Blockquote | Code | Html {html :: Text} | Anchor | Italic | Bold | Math {mathML :: Text}
+newtype Document = Doc [Element]
 
-data Data = Ast {ast :: AST} | Text {text :: Text}
+data Element = Heading Heading | Code Code | BlockQuote BlockQuote | List List | Table Table | HTML HTML | Paragraph Paragraph | BlankLine BlankLine
 
-data AST = AST {tag :: Tag, child :: [Data]}
+data Heading = H {level :: Int, text :: Text}
+
+data Code = C {language :: Text, code :: Text}
+
+newtype BlockQuote = Q Text
+
+data ListType = Ordered | Unordered
+
+data ListItem = LI {content :: Text, children :: [List]}
+
+data List = L {list_type :: ListType, items :: [ListItem]}
+
+data Table = T {header :: TableHeader, rows :: [TableRow]}
+
+-- TODO: layout/sizing info?
+newtype TableHeader = TH [Text]
+
+newtype TableRow = TR Text
+
+newtype HTML = Raw Text
+
+newtype Paragraph = P [InlineText]
+
+data InlineText = Normal Text | Bold InlineText | Italic InlineText | CodeLine Text | Link {nest :: InlineText, href :: Text}
+
+data BlankLine = BL
 
 -- for processing math
 -- https://hackage.haskell.org/package/typst-0.6.1/docs/Typst-Parse.html#v:parseTypst
--- app/Markdown.hs
+++ app/Markdown.hs
@@ -1,4 +1,41 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
 
-module Markdown () where
+module Markdown (markdownParser) where
 
+import Data.Text
+import IR
+import Text.Parsec
+import Text.Parsec.Combinator
+
+type Parser a = forall s u m t. (Stream s m t) => ParsecT s u m a
+
+markdownParser :: Parser Document
+markdownParser = Doc <$> many block
+
+block :: Parser Element
+block = choice [heading, codeBlock, quoteBlock, list, table, htmlBlock, paragraph, blankLine]
+
+heading :: Parser Element
+heading = pure $ Heading $ H {level = 1, text = ""}
+
+codeBlock :: Parser Element
+codeBlock = pure $ Code $ C {language = "", code = ""}
+
+quoteBlock :: Parser Element
+quoteBlock = pure $ BlockQuote $ Q ""
+
+list :: Parser Element
+list = pure $ List $ L {list_type = Ordered, items = []}
+
+table :: Parser Element
+table = pure $ Table $ T {header = TH [], rows = []}
+
+htmlBlock :: Parser Element
+htmlBlock = pure $ HTML $ Raw ""
+
+paragraph :: Parser Element
+paragraph = pure $ Paragraph $ P ""
+
+blankline :: Parser Element
+blankline = pure $ BlankLine BL
